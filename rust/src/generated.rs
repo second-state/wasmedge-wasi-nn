0f1d914ce9787f2a580b8f145fd290fc8b35ced4
PeterD1524 <qaz246135@gmail.com>
2024-06-22 01:00:15 +0800

    fix typo "NUEURALSPEED" and add some missing stuff for ggml and neural speed
    Signed-off-by: PeterD1524 <qaz246135@gmail.com>

--- rust/src/generated.rs
+++ rust/src/generated.rs
@@ -139,6 +139,8 @@ pub const GRAPH_ENCODING_TENSORFLOW: GraphEncoding = GraphEncoding(2);
 pub const GRAPH_ENCODING_PYTORCH: GraphEncoding = GraphEncoding(3);
 pub const GRAPH_ENCODING_TENSORFLOWLITE: GraphEncoding = GraphEncoding(4);
 pub const GRAPH_ENCODING_AUTODETECT: GraphEncoding = GraphEncoding(5);
+pub const GRAPH_ENCODING_GGML: GraphEncoding = GraphEncoding(6);
+pub const GRAPH_ENCODING_NEURALSPEED: GraphEncoding = GraphEncoding(7);
 pub const GRAPH_ENCODING_PIPER: GraphEncoding = GraphEncoding(11);
 impl GraphEncoding {
     pub const fn raw(&self) -> u8 {
@@ -153,7 +155,8 @@ impl GraphEncoding {
             3 => "PYTORCH",
             4 => "TENSORFLOWLITE",
             5 => "AUTODETECT",
-            7 => "NUEURALSPEED",
+            6 => "GGML",
+            7 => "NEURALSPEED",
             11 => "PIPER",
             _ => unsafe { core::hint::unreachable_unchecked() },
         }
@@ -166,6 +169,8 @@ impl GraphEncoding {
             3 => "",
             4 => "",
             5 => "",
+            6 => "",
+            7 => "",
             11 => "",
             _ => unsafe { core::hint::unreachable_unchecked() },
         }
